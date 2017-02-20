0566f23bdb450b6623b9a3a394335045df3da6f9
金樑樑 <jinliangliang@zhujia360.com>
2017-02-20 10:38:17 +0800

    First Commit

---
+++ app/src/main/java/com/jll/zoro/recycleview_3r/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jll.zoro.recycleview_3r;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
